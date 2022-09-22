c0acb2244d75477ed88877c4a136590df1f83cc7
torebjerkan530599 <tbj034@uit.no>
2022-09-22 21:03:43 +0200

    update : prepare MainActivity for Settings

--- app/src/main/java/com/example/turapp/MainActivity.kt
+++ app/src/main/java/com/example/turapp/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.turapp
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.turapp.databinding.ActivityMainBinding
 
 
@@ -16,6 +25,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val myToolbar = findViewById<View>(R.id.turToolbar) as Toolbar
+        setSupportActionBar(myToolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        this.menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.miSettings -> {
+                //startActivity(Intent(this, SettingActivity::class.java))
+            }
+            R.id.miHelp -> {
+                val alertDialog = AlertDialog.Builder(this@MainActivity).create()
+                alertDialog.setTitle("Help")
+                alertDialog.setMessage("Info info info")
+                alertDialog.setButton(
+                    AlertDialog.BUTTON_NEUTRAL, "OK"
+                ) { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+                alertDialog.show()
+            }
+            R.id.miInfo -> {
+                val toast = Toast.makeText(this, "Information...", Toast.LENGTH_LONG)
+                toast.show()
+            }
+            else -> {
+                val btnQuit = findViewById<Button>(R.id.miClose)
+                btnQuit.setOnClickListener {
+                    doQuit()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun doQuit() {
+        Toast.makeText(this, "Quitting!", Toast.LENGTH_SHORT).show()
 
     }
 }
