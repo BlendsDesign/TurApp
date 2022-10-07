c4a40778942c8259c6a8c8de1730546808869321
Magnus Ree <mre056@post.uit.no>
2022-10-07 12:25:39 +0200

    MapFragment: Cleanup imports

--- app/src/main/java/com/example/turapp/mapView/MapFragment.kt
+++ app/src/main/java/com/example/turapp/mapView/MapFragment.kt
@@ -1,19 +1,13 @@
 package com.example.turapp.mapView
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import com.example.turapp.R
 import com.example.turapp.databinding.FragmentMapBinding
-import com.example.turapp.startPage.StartFragmentDirections
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.example.turapp.mapView.MapViewModel
+
 
 
 class MapFragment : Fragment() {
