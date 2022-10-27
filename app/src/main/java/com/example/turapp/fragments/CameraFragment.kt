86c03de5ee8a0aab746bce5d0be986aa91e800e2
torebjerkan530599 <tbj034@uit.no>
2022-10-27 17:49:22 +0200

    add: camera

--- app/src/main/java/com/example/turapp/fragments/CameraFragment.kt
+++ app/src/main/java/com/example/turapp/fragments/CameraFragment.kt
@@ -1,22 +1,54 @@
 package com.example.turapp.fragments
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
 import android.os.Bundle
+import android.os.Handler
+import android.util.Size
+import android.view.*
+import android.view.TextureView.SurfaceTextureListener
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.example.turapp.viewmodels.CameraViewModel
 import com.example.turapp.databinding.FragmentCameraBinding
+import com.example.turapp.viewmodels.CameraViewModel
 
 
 class CameraFragment : Fragment() {
 
     private lateinit var binding : FragmentCameraBinding
+    private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    var cameraView: TextureView? = null
+    private var cameraId: String = ""
+    private var imageDimension: Size? = null
+    protected var cameraDevice: CameraDevice? = null
+    private var captureRequestBuilder //to get the feed
+            : CaptureRequest.Builder? = null
+    private var cameraCaptureSession: CameraCaptureSession? = null
+    private val mBackgroundHandler: Handler? = null
+//    private BufferedInputStream imageReader;
 
     private val viewModel: CameraViewModel by lazy {
         ViewModelProvider(this, CameraViewModel.Factory())[CameraViewModel::class.java]
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //camera permissions
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(requireActivity(), PERMISSIONS, 1)
+            return
+        }
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +59,102 @@ class CameraFragment : Fragment() {
 
         binding.viewModel = viewModel
 
+        cameraView = binding.cameraView
+        cameraView!!.surfaceTextureListener = cameraListener
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    var cameraListener: SurfaceTextureListener = object : SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
+            //eg. transform image capture size according to the surface width and height
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+    }
+
+    private fun openCamera() {
+        val manager = requireActivity().getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try { //in case the phone does not have a cam
+            cameraId = manager.cameraIdList[0] //0 is the camera on the back of the phone
+            val chts = manager.getCameraCharacteristics(cameraId)
+            val map = chts.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            imageDimension = map.getOutputSizes(SurfaceTexture::class.java)[0]
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            manager.openCamera(cameraId, stateCallback, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private val stateCallback: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            createCameraPreview()
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {}
+        override fun onError(camera: CameraDevice, error: Int) {
+            cameraDevice!!.close()
+            cameraDevice = null
+        }
+    }
+
+    private fun createCameraPreview() {
+        try {
+            val texture = cameraView!!.surfaceTexture!!
+            texture.setDefaultBufferSize(imageDimension!!.width, imageDimension!!.height)
+            val surface = Surface(texture)
+            captureRequestBuilder =
+                cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder!!.addTarget(surface)
+            cameraDevice!!.createCaptureSession(
+                listOf(surface),
+                object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        if (cameraDevice == null) return
+                        cameraCaptureSession = session
+                        updatePreview()
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        Toast.makeText(
+                            requireContext(), "Configuration changed", Toast.LENGTH_LONG
+                        ).show()
+                    }
+                },
+                null
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun updatePreview() { //update the modes for the capture request builder
+        if (cameraDevice == null) return
+        captureRequestBuilder!!.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+        try {
+            cameraCaptureSession!!.setRepeatingRequest(
+                captureRequestBuilder!!.build(),
+                null,
+                mBackgroundHandler
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
     }
 
 }
