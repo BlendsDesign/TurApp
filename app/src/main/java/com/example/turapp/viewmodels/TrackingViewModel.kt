643798bd847834fa7d30988fdea9e35f874a746f
Magnus Ree <mre056@post.uit.no>
2022-12-04 19:20:32 +0100

    TrackingViewModel.kt: Removed toast with altitude data

--- app/src/main/java/com/example/turapp/viewmodels/TrackingViewModel.kt
+++ app/src/main/java/com/example/turapp/viewmodels/TrackingViewModel.kt
@@ -160,8 +160,6 @@ class TrackingViewModel(private val app: Application) : ViewModel() {
             }
             .onEach { location ->
                 getLocation.postValue(location)
-                //TODO DELETE THIS SIMPLE TEST
-                _errorMessage.value = location.altitude.toString()
 
                 Log.d("TrackingViewModel", "Updated getLocation")
                 val geo = GeoPoint(location)
